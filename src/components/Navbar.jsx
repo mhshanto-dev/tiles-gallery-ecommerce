42c164a5df15aefaf9eab4ff82cefb454e2bf0c8
Md Mehedi Hasan Shanto <ll4146394@gmail.com>
2026-04-30 22:44:34 +0600

    Left: Website Logo (Navigates to Home).

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,10 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <div className="border-b px-2">
-      <nav className=" flex justify-between items-center  py-3 max-w-7xl mx-auto w-full">
-        <div className="flex gap-2 items-center">
+      <nav className="flex justify-between items-center py-3 max-w-7xl mx-auto w-full">
+        
+      
+        <Link href="/" className="flex gap-2 items-center">
           <Image
             src={"/logo.png"}
             alt="logo"
@@ -15,9 +17,12 @@ const Navbar = () => {
             height={30}
             className="object-cover h-auto w-auto"
           />
-          <h3 className="font-black text-lg text-gray-700">Tiles <span className="text-blue-800 text-3xl">G</span>allery</h3>
-        </div>
+          <h3 className="font-black text-lg text-gray-700">
+            Tiles <span className="text-blue-800 text-3xl">G</span>allery
+          </h3>
+        </Link>
 
+      
         <ul className="flex items-center gap-5 text-sm">
           <li>
             <Link href={"/"}>Home</Link>
@@ -30,8 +35,9 @@ const Navbar = () => {
           </li>
         </ul>
 
+        
         <div className="flex gap-4">
-          <ul className="flex items-center  text-sm">
+          <ul className="flex items-center text-sm gap-4">
             <li>
               <Link href={"/signup"}>SignUp</Link>
             </li>
@@ -40,6 +46,7 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
+
       </nav>
     </div>
   );
