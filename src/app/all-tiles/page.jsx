3beb0b8ce53e0e78c3767ff2fcdd8976dca53652
Md Mehedi Hasan Shanto <ll4146394@gmail.com>
2026-05-01 17:51:31 +0600

    successfully add all-tiles route data showing image adding give card and complete styles

--- src/app/all-tiles/page.jsx
+++ src/app/all-tiles/page.jsx
@@ -17,7 +17,7 @@ const AllTilesPage = async () => {
         <div>
             <h1 className="font-bold m-5 text-2xl">All Tiles</h1>
 
-            <div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
                 {data.tiles.map(tile=> <TilesCard key={tile.id} tile={tile} />)}
             </div>
         </div>
