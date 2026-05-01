6ebd4d15ffe157b522e1b0d16b6fba67745946dc
Md Mehedi Hasan Shanto <ll4146394@gmail.com>
2026-05-01 14:46:07 +0600

    create chips and style title price and viwes details butoon on the card

--- src/components/TilesCard.jsx
+++ src/components/TilesCard.jsx
@@ -16,21 +16,55 @@
 
 // export default TilesCard;
 
+import { Button, Card, Chip } from "@heroui/react";
 import Image from "next/image";
 
 const TilesCard = ({ tile }) => {
   return (
-    <div className="relative w-full h-[250px] rounded-lg overflow-hidden border border-gray-200">
+    <Card className="border">
+      <div className="relative w-full h-[250px] rounded-lg overflow-hidden border border-gray-200">
       <Image
         src={tile.image}
         alt={tile.title}
-        fill // 👈 Add this property
+        fill 
         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
         priority={true} 
         className="object-cover"
       />
+      <Chip size="large" className="absolute right-2 top-2 text-xl bg-amber-100">{tile.category}</Chip>
     </div>
+    <div>
+      <h2 className="font-semibold text-xl">{tile.title}</h2>
+    </div>
+   <div className="flex items-center justify-between">
+     <div>
+      <p className="font-bold text-gray-700">${tile.price}</p>
+    </div>
+
+    <div>
+      <Button className="font-bold text-gray-700">View Details</Button>
+    </div>
+   </div>
+
+    </Card>
   );
 };
 
 export default TilesCard;
+
+
+
+
+
+
+
+//  "id": "tile_001",
+//       "title": "Oida Ceramic Blue",
+//       "description": "Premium glossy blue ceramic tile with authentic texture",
+//       "image":"https://images.unsplash.com/photo-1708540084677-dc5838b37627",
+//       "category": "ceramic",
+//       "price": 45.99,
+//       "currency": "USD",
+//       "dimensions": "60x60 cm",
+//       "material": "Ceramic",
+//       "inStock": true
