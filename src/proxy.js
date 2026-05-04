4c546067c2b0f1c94ad9d4689ba542210487901b
Md Mehedi Hasan Shanto <ll4146394@gmail.com>
2026-05-04 12:27:34 +0600

    error handaling

--- src/proxy.js
+++ src/proxy.js
@@ -1,27 +1,53 @@
-// src/middleware.js
-import { headers } from 'next/headers'
-import { NextResponse } from 'next/server'
-import { auth } from "@/lib/auth"
 
-export async function middleware(request) {
-   
-    const session = await auth.api.getSession({
-        headers: await headers()
-    })
+// import { headers } from 'next/headers';
+// import { NextResponse } from 'next/server';
+// import { auth } from './lib/auth'; 
 
-   
-    if (!session) {
-        return NextResponse.redirect(new URL('/signin', request.url))
+// export async function proxy(request) {
+//     try {
+//         const session = await auth.api.getSession({
+//             headers: await headers()
+//         });
+
+//         if (!session) {
+            
+//             return NextResponse.redirect(new URL('/signin', request.url));
+//         }
+
+//         return NextResponse.next();
+//     } catch (error) {
+        
+//         console.error("Proxy Auth Error:", error);
+//         return NextResponse.next();
+//     }
+// }
+
+// export const config = {
+//     matcher: [
+//         '/profile', 
+//         '/all-tiles/:path' 
+//     ],
+// };
+
+
+
+// src/proxy.js
+import { NextResponse } from 'next/server';
+import { auth } from './lib/auth'; 
+
+export async function proxy(request) {
+    try {
+       
+        const session = await auth.api.getSession({
+            headers: request.headers 
+        });
+
+        if (!session && (request.nextUrl.pathname === '/profile' || request.nextUrl.pathname === '/all-tiles')) {
+            return NextResponse.redirect(new URL('/signin', request.url));
+        }
+
+        return NextResponse.next();
+    } catch (e) {
+        return NextResponse.next();
     }
-
-  
-    return NextResponse.next()
-}
-
-
-export const config = {
-    matcher: [
-        '/profile', 
-        '/all-tiles' 
-    ],
 }
