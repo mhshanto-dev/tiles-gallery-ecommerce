4105206908178eb1e17841fd7ebdcdf52a7639e1
Md Mehedi Hasan Shanto <ll4146394@gmail.com>
2026-05-04 11:31:27 +0600

    Add middleware for route protection and product details access

---
+++ src/middleware.js
@@ -0,0 +1,25 @@
+import { headers } from 'next/headers'
+import { NextResponse } from 'next/server'
+import { auth } from "@/lib/auth"
+
+export async function middleware(request) {
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+
+    
+    if (!session) {
+        return NextResponse.redirect(new URL('/Signin', request.url))
+    }
+
+    return NextResponse.next()
+}
+
+
+export const config = {
+    matcher: [
+        '/profile', 
+        '/all-tiles', 
+        '/product/:path*' 
+    ],
+}
